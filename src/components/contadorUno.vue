1d11adb070b4f9e5af7774008863af58ed68fe2f
Kevin-022C <keyfrans96@gmail.com>
2025-11-23 15:14:31 -0500

    Taller 13: Componentes de Vue.js

---
+++ src/components/contadorUno.vue
@@ -0,0 +1,30 @@
+<template>
+    
+    <div>
+    
+        <h1>Universidad Central del Ecuador</h1>
+        <h2>Carrera de computación</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ContadorUno'
+}
+</script>
+
+<style>
+h1 {
+    font-family: 'Times New Roman', Times, serif;
+    color: rgb(29, 26, 200);
+    
+}
+div {
+    display: flex;
+    height: auto;
+    flex-direction: column;
+    justify-items: center;
+    border-radius: 25px;
+    background-color: bisque;
+}
+</style>
---
+++ src/view/ContadorView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ContadorUno/>
+    </div>
+</template>
+
+<script>
+import ContadorUno from '../components/contadorUno.vue';
+
+export default {
+  name: 'ContadorView',
+  components: {
+    ContadorUno
+  }
+}
+</script>
+
+<style>
+</style>
